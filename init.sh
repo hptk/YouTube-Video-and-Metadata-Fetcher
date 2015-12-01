ebc03f212070f7f6f8671c70f434d8d7f1023c51
hptk <hanspetter92@gmail.com>
2015-12-01 19:31:51 +0100

    fixed folder init

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 pip2.7 install -r requirements.txt
-mkdir data
+if [ ! -d "data" ]; then
+    mkdir data
+fi
 python2.7 manage.py create_db
 python2.7 manage.py db init
 python2.7 manage.py db migrate
